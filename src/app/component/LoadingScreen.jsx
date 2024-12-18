a63f8e86f62b485f65392cbcf680f1635c6d5f5c
Hyehe <khl99122412@gmail.com>
2024-12-18 12:11:32 +0900

    12/18 수정중중

---
+++ src/app/component/LoadingScreen.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function LoadingScreen(props) {
+  return (
+    <div className="loading-screen">
+      <img src="/images/weipan.jpg" 
+      alt="Loading Icon" className="loading-image" />
+    </div>
+  );
+}
+
+export default LoadingScreen;
